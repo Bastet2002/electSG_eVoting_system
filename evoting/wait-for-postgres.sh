e3b60e87085709bab77012b5fdfb8b665fcde066
Loh Chin Yee <percussionist8896@gmail.com>
2024-05-14 15:56:40 +0800

    [002] switch to postgres

---
+++ evoting/wait-for-postgres.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$DJANGO_DB_PASSWORD psql -h "$host" -U "$DJANGO_DB_USER" -d "$DJANGO_DB_NAME" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
