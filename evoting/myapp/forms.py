5aa2181c85ff929bc3265c657f9bd6fe8fd1a70c
Nigel <fatfark@LAPTOP-531M1QBM>
2024-05-18 16:32:00 +0800

    [2] Changed models of district and user account

--- evoting/myapp/forms.py
+++ evoting/myapp/forms.py
@@ -1,37 +1,47 @@
 from django import forms
-
+from .models import UserAccount, District
+    
 roles = [
     ('user', 'User'),
     ('candidate', 'Candidate'),
     ('admin', 'Admin'),
 ]
 
-election_districts = [
-    ('yio chu kang', 'Yio Chu Kang'),
-    ('ang mo kio', 'Ang Mo Kio'),
-    ('bishan', 'Bishan'),
-]
+# election_districts = [
+#     ('yio chu kang', 'Yio Chu Kang'),
+#     ('ang mo kio', 'Ang Mo Kio'),
+#     ('bishan', 'Bishan'),
+# ]
 
 election_status = [
     ('polling day', 'Polling Day'),
     ('cool off day'), ('Cool Off Day'),
 ]
 
-class createNewUser(forms.Form):
-    name = forms.CharField(label="Name", max_length=200)
-    date_of_birth = forms.DateField(label="Date of Birth")
-    id = forms.IntegerField(label="ID")
-    password = forms.CharField(label="Password", max_length=200, widget=forms.PasswordInput)
-    district = forms.CharField(label="District", widget=forms.Select(choices=election_districts))
-    role = forms.CharField(label="Role", widget=forms.Select(choices=roles))
+class createNewUser(forms.ModelForm):  # Class names typically start with a capital letter
+    class Meta:
+        model = UserAccount
+        fields = ['name', 'date_of_birth', 'user_id', 'password', 'district', 'role']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
 
 class createProfile(forms.Form):
     profile_name = forms.CharField(label="Profile Name", max_length=200)
     description = forms.CharField(label="Description",widget=forms.Textarea())
 
 class addDistrict(forms.Form):
-    district_name = forms.CharField(label="District",widget=forms.Textarea())
-    comments = forms.CharField(label="Comments", required=False)
+    district_names = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Enter district names separated by semicolons (;)'}),
+        help_text="Enter multiple district names separated by semicolons (;)"
+    )
+
+class editDistrict(forms.ModelForm):
+    class Meta:
+        model = District
+        fields = ['name']
+
 
 class createAnnouncement(forms.Form):
     header = forms.CharField(label="Header", max_length=200)
