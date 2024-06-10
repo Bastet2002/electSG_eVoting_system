e318144878b743c11cfc03d81a9bf8b96e5eef75
Loh Chin Yee <percussionist8896@gmail.com>
2024-06-10 15:32:26 +0800

    [2] correct the db schema

--- evoting/dbinit/db_init.sql
+++ evoting/dbinit/db_init.sql
@@ -9,17 +9,17 @@ drop table if exists Vote_results;
 
 -- can have district_id for simplicity
 CREATE TABLE Singpass_User (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(9) PRIMARY KEY,
     district VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
+    phone VARCHAR(20) NOT NULL UNIQUE,
     DOB DATE NOT NULL
 );
 
 CREATE TABLE Voter (
     voter_id SERIAL PRIMARY KEY,
     district_id BIGINT NOT NULL,
-    hashfrominfo VARCHAR(64) UNIQUE, -- A 512bit hash, with H(disctrict_id, name, phone, DOB)
+    hashfrominfo VARCHAR(64) UNIQUE, -- A 512bit hash, with H(disctrict_id, id, name, phone, DOB)
     pkV VARCHAR(32) NOT NULL,
     FOREIGN KEY (district_id) REFERENCES myapp_district(id) -- no delete cascade
 );
@@ -33,8 +33,8 @@ CREATE TABLE Voter_SecretKey (
 
 CREATE TABLE Candidate_PublicKey (
     candidate_id BIGINT PRIMARY KEY,
-    pkV VARCHAR(255) NOT NULL,
-    pkS VARCHAR(255) NOT NULL,
+    pkV VARCHAR(32) NOT NULL UNIQUE,
+    pkS VARCHAR(32) NOT NULL UNIQUE,
     FOREIGN KEY (candidate_id) REFERENCES myapp_useraccount(id) ON DELETE CASCADE
 );
 
@@ -47,8 +47,9 @@ CREATE TABLE Candidate_SecretKey (
 
 CREATE TABLE Vote_Records (
     key_image VARCHAR(32) PRIMARY KEY,
-    district_id VARCHAR(3) NOT NULL,
-    transaction_record JSONB NOT NULL
+    district_id BIGINT NOT NULL,
+    transaction_record JSONB NOT NULL, 
+    FOREIGN KEY (district_id) REFERENCES myapp_district(id) -- no delete cascade
 );
 
 CREATE TABLE Vote_Results (
