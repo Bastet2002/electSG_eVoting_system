7e84f1f1a9936e3f87799b5cdc63263e8a85f3e0
Phyo Wai Lin <phyowailin1718@gmail.com>
2024-07-07 02:34:21 +0800

    [9] tested h2s, two add key functions

--- evoting/ringct/test/test_hash.cpp
+++ evoting/ringct/test/test_hash.cpp
@@ -1,5 +1,6 @@
 #include "../rct/rctType.h"
 #include "../rct/rctOps.h"
+#include "test_util.h"
 #include <catch2/catch_all.hpp>
 #include <filesystem>
 #include <fstream>
@@ -23,42 +24,30 @@ TEST_CASE( "test case", "[tag]" ) {
     }
 }
 
-
-// Behaviour-driven development
-SCENARIO( "test case scenario ", "[tag]" ) {
-    GIVEN( "Given the arranged variable" ) {
-
-        // Arrange variables with assertions
-        var = 5
-        REQUIRE( var == 5 );
-
-        WHEN( "When the action is taken" ) {
-            // var will be reinitiated in every when block
-            // you can also nested when block
-
-            // Action
-            var = 10
-
-            THEN( "Then the expected result should be" ) {
-                // Assert
-                REQUIRE( var == 10 );
-            }
-        }
-    }
-}
 */
+// Behaviour-driven development
+// SCENARIO( "test case scenario ", "[tag]" ) {
+//     GIVEN( "Given the arranged variable" ) {
 
-vector<string> tokeniser(const string &aline, const char delimeter = ' ')
-{
-    vector<string> tokens;
-    string token;
-    istringstream input(aline);
-    while (getline(input, token, delimeter))
-    {
-        tokens.push_back(token);
-    }
-    return tokens;
-}
+//         // Arrange variables with assertions
+//         var = 5
+//         REQUIRE( var == 5 );
+
+//         WHEN( "When the action is taken" ) {
+//             // var will be reinitiated in every when block
+//             // you can also nested when block
+
+//             // Action
+//             var = 10
+
+//             THEN( "Then the expected result should be" ) {
+//                 // Assert
+//                 REQUIRE( var == 10 );
+//             }
+//         }
+//     }
+// }
+
 
 SCENARIO("Test the consistency of the hash to point function", "[hash]")
 {
@@ -82,8 +71,7 @@ SCENARIO("Test the consistency of the hash to point function", "[hash]")
 
             if (aline == "x")
             {
-                input_hex_byte.resize(1);
-                hex_to_bytearray(input_hex_byte.data(), "00");
+                input_hex_byte.resize(0);
             }
             else
             {
@@ -155,6 +143,86 @@ SCENARIO("Test the consistency of the hash to point function", "[hash]")
                         cout << "String: " << tokens[1] << endl;
                         print_hex(actual, 32);
                         print_hex(expected, 32);
+                    }
+                    REQUIRE(is_equal);
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Test the consistency of the hash to scalar function", "[hash]") {
+    // base hex string of variety length for use
+    const string base_file = filesystem::absolute("/app/test/text/hash_infile");
+    // contain expected output
+    const string h2s_file = filesystem::absolute("/app/test/text/h2s.txt");
+
+    GIVEN("Any inputs to the hash to scalar function") {
+        ifstream infile(base_file);
+        REQUIRE(infile.is_open());
+        string aline;
+        int i = 0;
+        while (getline(infile, aline)) {
+            vector<BYTE> input_hex_byte;
+            input_hex_byte.resize(aline.size() / 2);
+            if (aline == "x") {
+                input_hex_byte.resize(0);
+            } else {
+                hex_to_bytearray(input_hex_byte.data(), aline);
+            }
+            i++;
+
+            WHEN("it produces the output scalar " + to_string(i)) {
+                BYTE h2s[32];
+                hash_to_scalar(h2s, input_hex_byte.data(), input_hex_byte.size());
+
+                THEN("the output should be a valid scalar for ed25519") {
+                    REQUIRE(sc25519_is_canonical(h2s) == 1);
+                    REQUIRE(sodium_is_zero(h2s, crypto_core_ed25519_SCALARBYTES) == 0);
+                }
+
+                WHEN("the same input is recomputed again " + to_string(i)) {
+                    BYTE h2s_recomputed[crypto_core_ed25519_SCALARBYTES];
+                    hash_to_scalar(h2s_recomputed, input_hex_byte.data(), input_hex_byte.size());
+
+                    THEN("both outputs should be equal") {
+                        REQUIRE(sodium_memcmp(h2s, h2s_recomputed, crypto_core_ed25519_SCALARBYTES) == 0);
+                    }
+                }
+            }
+        }
+    }
+
+    GIVEN("The actual output should match the expected input") {
+        ifstream infile(h2s_file);
+        REQUIRE(infile.is_open());
+        string aline;
+        int i = 0;
+        while (getline(infile, aline)) {
+            vector<string> tokens = tokeniser(aline);
+            BYTE expected[crypto_core_ed25519_SCALARBYTES];
+            hex_to_bytearray(expected, tokens[0]);
+            i++;
+
+            vector<BYTE> input_hex_byte;
+            input_hex_byte.resize(tokens[1].size() / 2);
+            if (tokens[1] == "x") {
+                input_hex_byte.resize(0);
+            } else {
+                hex_to_bytearray(input_hex_byte.data(), tokens[1]);
+            }
+
+            WHEN("the known input is passed to the function" + to_string(i)) {
+                BYTE actual[crypto_core_ed25519_SCALARBYTES];
+                hash_to_scalar(actual, input_hex_byte.data(), input_hex_byte.size());
+
+                THEN("the expected output is matched with output") {
+                    bool is_equal = sodium_memcmp(actual, expected, crypto_core_ed25519_SCALARBYTES) == 0;
+                    if (!is_equal) {
+                        cout << "Actual and expected output does not match" << endl;
+                        cout << "String: " << tokens[1] << endl;
+                        print_hex(actual, crypto_core_ed25519_SCALARBYTES);
+                        print_hex(expected, crypto_core_ed25519_SCALARBYTES);
                     }
                     REQUIRE(is_equal);
                 }
