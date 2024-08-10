5c260b9ee18bac1d19149ff5b05f632aefdbef26
Loh Chin Yee <percussionist8896@gmail.com>
2024-08-10 18:33:39 +0800

    [99] update config for cicd

--- evoting/wait-for-postgres.sh
+++ evoting/wait-for-postgres.sh
@@ -41,7 +41,7 @@ command psql $DATABASE_URL -f ./ringct/dbinit/db_init.sql
 command python manage.py makemigrations 
 command python manage.py migrate 
 
-if ["$ENVIRONMENT" = "test"]; then 
+if [ "$ENVIRONMENT" = "test" ]; then 
   echo "Running in cicd env"
   command python /app/manage.py test myapp.tests.test_models
   command python /app/manage.py test myapp.tests.test_urls
